71507dcc38b5a1153c2539c88258399c60ff812e
Jake Ward <jakenicholasward@gmail.com>
2025-01-15 14:41:28 -0500

    update capitals task to work with arbitrary n

--- tasks/capitals.py
+++ tasks/capitals.py
@@ -85,54 +85,68 @@ NAMES = [
 ]
 
 
-def capitals_generator():
+def capitals_generator(n=2):
+    assert n * 2 < len(NAMES)
+    assert n * 2 < len(CAPITAL_MAP)
     while True:
-        E_0, E_1, E_0p, E_1p = np.random.choice(NAMES, 4, replace=False)
-        A_0, A_1, A_0p, A_1p = np.random.choice(
-            list(CAPITAL_MAP.keys()), 4, replace=False
-        )
-        yield CapitalsExample(E_0, E_1, A_0, A_1, E_0p, E_1p, A_0p, A_1p)
+        entities = np.random.choice(NAMES, n * 2, replace=False)
+        attributes = np.random.choice(list(CAPITAL_MAP.keys()), n * 2, replace=False)
+        yield CapitalsExample(n, entities, attributes)
 
 
 class CapitalsExample:
     def __init__(
         self,
-        E_0: str,
-        E_1: str,
-        A_0: str,
-        A_1: str,
-        E_0p: str,
-        E_1p: str,
-        A_0p: str,
-        A_1p: str,
+        n: int,
+        entities: list[str],
+        attributes: list[str],
     ):
-        self.E_0 = E_0
-        self.E_1 = E_1
-        self.E_0p = E_0p
-        self.E_1p = E_1p
-        self.A_0 = A_0
-        self.A_1 = A_1
-        self.A_0p = A_0p
-        self.A_1p = A_1p
-        self.answer_0 = CAPITAL_MAP[A_0]
-        self.answer_1 = CAPITAL_MAP[A_1]
-        self.answer_0p = CAPITAL_MAP[A_0p]
-        self.answer_1p = CAPITAL_MAP[A_1p]
+        self.n = n
+        self.entities = entities[:n]
+        self.entities_p = entities[n:]
+        self.attributes = attributes[:n]
+        self.attributes_p = attributes[n:]
+        self.answers = [CAPITAL_MAP[a] for a in self.attributes]
+        self.answers_p = [CAPITAL_MAP[a] for a in self.attributes_p]
 
-    @staticmethod
-    def _context(E_0: str, E_1: str, A_0: str, A_1: str) -> str:
-        context = f"""\
+        for i, entity in enumerate(self.entities):
+            setattr(self, f"E_{i}", entity)
+            setattr(self, f"query_E_{i}", self._query(entity))
+
+        for i, entity_p in enumerate(self.entities_p):
+            setattr(self, f"E_{i}p", entity_p)
+            setattr(self, f"query_E_{i}p", self._query(entity_p))
+
+        for i, (attribute, answer) in enumerate(zip(self.attributes, self.answers)):
+            setattr(self, f"A_{i}", attribute)
+            setattr(self, f"answer_{i}", answer)
+
+        for i, (attribute_p, answer_p) in enumerate(
+            zip(self.attributes_p, self.answers_p)
+        ):
+            setattr(self, f"A_{i}p", attribute_p)
+            setattr(self, f"answer_{i}p", answer_p)
+
+    def _context(self, is_p) -> str:
+        context = """\
 Answer the question based on the context below. Keep the answer short.
-Context: {E_0} lives in the capital city of {A_0}. {E_1} lives in the capital city of {A_1}."""
+Context:"""
+        es_ = self.entities_p if is_p else self.entities
+        as_ = self.attributes_p if is_p else self.attributes
+        # iterate every 2:
+        for i in range(self.n):
+            E = es_[i]
+            A = as_[i]
+            context += f""" {E} lives in the capital city of {A}."""
         return context
 
     @property
     def context(self) -> str:
-        return self._context(self.E_0, self.E_1, self.A_0, self.A_1)
+        return self._context(is_p=False)
 
     @property
     def context_p(self) -> str:
-        return self._context(self.E_0p, self.E_1p, self.A_0p, self.A_1p)
+        return self._context(is_p=True)
 
     @staticmethod
     def _query(qn_subject) -> str:
@@ -140,19 +154,3 @@ Context: {E_0} lives in the capital city of {A_0}. {E_1} lives in the capital ci
         return f"""
 Question: Which city does {qn_subject} live in?
 Answer: {qn_subject} lives in the city of"""
-
-    @property
-    def query_E_0(self) -> str:
-        return self._query(self.E_0)
-
-    @property
-    def query_E_1(self) -> str:
-        return self._query(self.E_1)
-
-    @property
-    def query_E_0p(self) -> str:
-        return self._query(self.E_0p)
-
-    @property
-    def query_E_1p(self) -> str:
-        return self._query(self.E_1p)
